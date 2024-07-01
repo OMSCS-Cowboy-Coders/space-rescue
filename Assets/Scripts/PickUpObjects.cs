22f028237b231feaa3afc7d3848c65abf2b6d6f7
indugh <164970528+indugh@users.noreply.github.com>
2024-06-30 20:06:27 -0400

    Merging changing to get spaceship parts to homebase (#25)
    * able to pickup parts
    
    * Added ability to pickup and put down parts
    
    * Made a prefab
    
    * removed terrain
    
    ---------
    
    Co-authored-by: Indu Ghandikota <ighandikota3@gatech.edu>

---
+++ Assets/Scripts/PickUpObjects.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class PickUpObjects : MonoBehaviour
+{
+
+    public PlayerController playerController;
+    private Rigidbody itemRB;
+
+    private Collider itemCollider;
+
+    public GameObject player, itemContainerForPlayer;
+
+    public bool itemIsBeingHeldByPlayer;
+
+    public static bool playerIsHoldingItemGlobal;
+
+    private bool playerIsTryingToPickUpObject;
+
+    public float farthestDistanceFromItem = 2f;
+
+    private bool pickUpItem = false; 
+
+  
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemRB = this.GetComponent<Rigidbody>();
+        itemCollider = this.GetComponent<Collider>();
+
+        if(itemRB != null && itemCollider != null) {
+            if(!itemIsBeingHeldByPlayer) {
+                itemRB.isKinematic = false;
+                itemCollider.isTrigger = false;
+            } else {
+                itemRB.isKinematic = false;
+                itemCollider.isTrigger = false;
+                playerIsHoldingItemGlobal = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        float distanceFromItem = 0f;
+        distanceFromItem = Vector3.Distance(player.transform.position, this.transform.position);
+
+         if (!itemIsBeingHeldByPlayer &&
+         Input.GetKeyDown(KeyCode.E) &&
+          !playerIsHoldingItemGlobal &&
+          distanceFromItem <= farthestDistanceFromItem) {
+            Debug.Log("In this call to pickup the object");
+            playerIsTryingToPickUpObject = true;
+            playerController.anim.SetBool("PickingUpObject", true);
+            playerController.anim.SetBool("PuttingDownObject", false);
+            playerController.objectToCarry = gameObject;
+            
+        } else if(itemIsBeingHeldByPlayer && Input.GetKeyDown(KeyCode.Q)) {
+            playerIsTryingToPickUpObject = false;
+            playerController.anim.SetBool("PuttingDownObject", true);
+            playerController.anim.SetBool("CarryingObject", false);
+            PutDownObject();
+        }
+    }
+
+    public void setVarsForPickUp() {
+        Debug.Log("In pick up item");
+       
+        itemIsBeingHeldByPlayer = true;
+        playerIsHoldingItemGlobal = true;
+
+        this.transform.SetParent(itemContainerForPlayer.transform);
+        // itemContainerForPlayer.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+        // this.transform.localPosition = Vector3.zero;
+        // this.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        // this.transform.localScale = Vector3.one;
+
+        // itemRB.isKinematic = true;
+        // itemCollider.isTrigger = true;
+    }
+
+    public void PutDownObject() {
+        itemIsBeingHeldByPlayer = false;
+        playerIsHoldingItemGlobal = false;
+
+        this.transform.SetParent(null);
+        
+        itemRB.velocity = player.GetComponent<Rigidbody>().velocity;
+
+        itemRB.isKinematic = false;
+        itemCollider.isTrigger = false;
+        
+        GameObject batteryStorage = GameObject.FindWithTag("BatteryStorage");
+        float closeToBatteryStorage = Vector3.Distance(this.transform.position, batteryStorage.transform.position);
+        if (closeToBatteryStorage < 2f) {
+            this.transform.SetParent(GameObject.FindWithTag("BatteryStorage").transform);
+            playerController.updateNumBatteriesRetrieved();
+        }
+    }
+
+}
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody astronautRigidBody;
 
-    private Animator anim;
+    public Animator anim;
 
     private float astronautX;
     private float astronautY;
@@ -20,14 +21,26 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 newLocation;
 
-    private Quaternion newRotation = Quaternion.identity;
-
-    Vector3 m_EulerAngleVelocity;
-
     public float moveSpeed;
     public float moveAnimSpeed;
 
     private PlayerMetrics playerMetrics;
+
+    private bool playerIsTryingToPickUpObject;
+
+    public GameObject objectToCarry;
+
+    public int groundContactCount;
+
+    public float jumpableGroundNormalMaxAngle = 45f;
+    public bool closeToJumpableGround;
+
+    public GameObject itemContainerForPlayer;
+
+
+      public bool retrievedAllParts = false;
+      public int numPartsRecieved = 0;
+
 
     private FootstepsController footstepsController;
 
@@ -54,11 +67,19 @@ public class PlayerController : MonoBehaviour
 
     }
 
+   
     void FixedUpdate() {
         anim.SetFloat("Y_movement", astronautY);
 
         newLocation.Set(astronautX, 0f, astronautY);
         newLocation.Normalize();
+
+        if(numPartsRecieved == 5) {
+            // pause the game
+            // Show Win text
+            Debug.Log("You've won!");
+
+        }
 
         if (Mathf.Abs(astronautX) > 0.1f || Mathf.Abs(astronautY) > 0.1f)
         {
@@ -69,7 +90,9 @@ public class PlayerController : MonoBehaviour
     public float turnRate = 100f;
     void OnAnimatorMove()
     {
-        Vector3 newRootPosition = Vector3.LerpUnclamped(astronautRigidBody.transform.position, anim.rootPosition, moveSpeed);
+        Vector3 newRootPosition;
+    
+        newRootPosition = Vector3.LerpUnclamped(astronautRigidBody.transform.position, anim.rootPosition, moveSpeed);
         astronautRigidBody.MovePosition(newRootPosition);
 
         var rot = Quaternion.AngleAxis(turnRate * astronautX * Time.deltaTime, Vector3.up);
@@ -95,4 +118,95 @@ public class PlayerController : MonoBehaviour
             playerMetrics.useSprintPowerup();
         }
     }
+
+     void OnAnimatorIK(int layerIndex) {
+        float itemWeight = 1.0f;
+        if(anim) {
+            
+            AnimatorStateInfo astate = anim.GetCurrentAnimatorStateInfo(0);
+         
+            if(astate.IsName("picking_up")) {
+                Debug.Log("trying to pick up");
+                itemWeight = anim.GetFloat("ItemWeight");
+                // Set the target position, if one has been assigned
+                if(objectToCarry != null) {
+                    anim.SetLookAtWeight(itemWeight);
+                    anim.SetLookAtPosition(objectToCarry.transform.position);
+                    anim.SetIKPositionWeight(AvatarIKGoal.RightHand,itemWeight);
+                    anim.SetIKPosition(AvatarIKGoal.RightHand, objectToCarry.transform.position);
+                    Debug.Log("finished setting up things");
+                }
+            } else {
+                anim.SetIKPositionWeight(AvatarIKGoal.RightHand,0);
+                anim.SetLookAtWeight(0);
+            }
+        }
+    }
+
+    public void PickUpItem() {
+        Debug.Log("In pick up item");
+        
+        objectToCarry.transform.SetParent(itemContainerForPlayer.transform);
+
+        PickUpObjects pickUpObjects = objectToCarry.GetComponent<PickUpObjects>();
+
+        Collider c = objectToCarry.GetComponent<Collider>();
+        Rigidbody rb = objectToCarry.GetComponent<Rigidbody>();
+       
+        pickUpObjects.setVarsForPickUp();
+    
+        objectToCarry.transform.localPosition = Vector3.zero;
+        objectToCarry.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        this.transform.localScale = Vector3.one;
+
+        rb.isKinematic = true;
+        c.isTrigger = true;
+    }
+
+    public void PutDownItem() {
+         Debug.Log("In pick up item");
+         
+        objectToCarry.transform.SetParent(itemContainerForPlayer.transform);
+
+        PickUpObjects pickUpObjects = objectToCarry.GetComponent<PickUpObjects>();
+
+        Collider c = objectToCarry.GetComponent<Collider>();
+        Rigidbody rb = objectToCarry.GetComponent<Rigidbody>();
+       
+    
+        objectToCarry.transform.SetParent(null);
+        objectToCarry.transform.SetParent(GameObject.FindWithTag("BatteryStorage").transform);
+        // this.transform.localScale = Vector3.one;      
+    }
+
+    public void setPickUpAnimToFalse() {
+        anim.SetBool("PickingUpObject", false);
+        anim.SetBool("CarryingObject", true);
+    }
+
+    void OnCollisionEnter(Collision c) {
+        Debug.Log("Tag: " + c.transform.gameObject.tag);
+        if (c.transform.gameObject.tag == "MarsFloor")
+        { 
+            ++groundContactCount;
+        } else if (c.transform.gameObject.tag == "SpaceshipPart") {
+
+        }
+        astronautRigidBody.velocity = Vector3.zero;
+    }
+
+     private void OnCollisionExit(Collision collision)
+    {
+
+        if (collision.transform.gameObject.tag == "MarsFloor")
+        {
+            --groundContactCount;
+        }
+
+    }
+
+    public void updateNumBatteriesRetrieved() {
+        numPartsRecieved = numPartsRecieved + 1;
+    }
+
 }
