d7eabc3bffebf5eff2d3d980a142f8afb9bd8f8a
indugh <164970528+indugh@users.noreply.github.com>
2024-07-16 06:28:51 -0400

    Fixing pickup - No animation (#57)
    * Moving things around to only use 1 terrain tile
    
    * making sure when player puts down object its always in front of the player
    
    * Some slight adjustments
    
    ---------
    
    Co-authored-by: Indu Ghandikota <ighandikota3@gatech.edu>

--- Assets/Scripts/PickUpObjects.cs
+++ Assets/Scripts/PickUpObjects.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -111,9 +112,11 @@ public class PickUpObjects : MonoBehaviour
 
         itemContainerForPlayer.transform.localScale = Vector3.one;
         this.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-        this.transform.position = player.transform.position;
-        Debug.Log("Player position: " + player.transform.position);
-        Debug.Log("Object postion: " + this.transform.position);
+        Debug.Log("Player forward: " + player.transform.forward);
+        Debug.Log("player postion: " + player.transform.position);
+        float multiplier = player.transform.forward.z > 0 ? 1 : -1;
+        this.transform.position = player.transform.position + new Vector3(0,0, .5f * multiplier);
+        
 
          itemRB.isKinematic = false;
         itemCollider.isTrigger = false;
@@ -125,6 +128,14 @@ public class PickUpObjects : MonoBehaviour
             playerController.updateNumBatteriesRetrieved();
         }
         Debug.Log("Object postion 2: " + this.transform.position);
+    }
+
+    void OnCollisionEnter(Collision c) {
+        itemRB.velocity = Vector3.zero;
+    }
+    void OnCollisionExit(Collision c) {
+        itemRB.velocity = Vector3.zero;
+        c.rigidbody.velocity = Vector3.zero;
     }
 
 }
