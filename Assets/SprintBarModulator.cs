6bc34117175d75a8b33703693fbacaecd199c382
jantanja <tanjannibal@gmail.com>
2024-07-18 20:57:43 -0700

    Sprint Bar

---
+++ Assets/SprintBarModulator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SprintBarModulator : MonoBehaviour
+{
+    private PlayerMetrics playerMetrics;
+    public RectTransform sprintFill;
+    public float initialSprintFillWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMetrics = FindObjectOfType<PlayerMetrics>();
+        initialSprintFillWidth = sprintFill.sizeDelta.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float sprintLeft = playerMetrics.getSprintEnergy();
+        updateSprintBar(sprintLeft);
+        // print("This is sprint left: " + sprintLeft);
+    }
+    void updateSprintBar(float sprintLeft) {
+        float percentage = sprintLeft / 100; // sprintFill is expressed as a percentage.
+        print("percentage: " + percentage);
+        print("initialSprintFillWidth: " + initialSprintFillWidth);
+        print(" THIS IS HOW WIDE WE SHOULD BE " + initialSprintFillWidth * percentage);
+        float width = Mathf.Min(initialSprintFillWidth * percentage, initialSprintFillWidth);
+        sprintFill.sizeDelta = new Vector2(width, sprintFill.sizeDelta.y);
+    }
+}
