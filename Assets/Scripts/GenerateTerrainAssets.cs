2a8d412a8db4ce9241713e2aa56cac876442eb1e
Christian Tran <ctran4347@gmail.com>
2024-07-20 11:42:52 -0500

    Generating terrain assets

--- Assets/Scripts/GenerateTerrainAssets.cs
+++ Assets/Scripts/GenerateTerrainAssets.cs
@@ -6,12 +6,37 @@ public class GenerateTerrainAssets : MonoBehaviour
 {
     // Start is called before the first frame update
     
-    public GameObject[] terrainPrefabs;
+    float treeScaleMin = 1f;
+    float treeScaleMax = 3f;
+    float treeAmountMin = 1000f;
+    float treeAmountMax = 2000f;
+    public GameObject[] treePrefabs;
+
+    float rockScaleMin = 0.1f;
+    float rockScaleMax = 1f;
+    float rockAmountMin = 500f;
+    float rockAmountMax = 1000f;
+    public GameObject[] rockPrefabs;
+
+    float plantScaleMin = 1f;
+    float plantScaleMax = 10f;
+    float plantAmountMin = 750f;
+    float plantAmountMax = 1500f;
+    public GameObject[] plantPrefabs;
+    
     private GameObject TerrainAssetsParent;
     void Start()
     {
         TerrainAssetsParent = new GameObject("TerrainAssetsParent");
-        GenerateAssets();
+        //Generate tree prefabs
+        float treeAmount = Random.Range(treeAmountMin, treeAmountMax);
+        GenerateAssets(treePrefabs,treeAmount, treeScaleMin, treeScaleMax);
+        //Generate rock prefabs
+        float rockAmount = Random.Range(rockAmountMin, rockAmountMax);
+        GenerateAssets(rockPrefabs, rockAmount, rockScaleMin, rockScaleMax);
+        //Generate rock prefabs
+        float plantAmount = Random.Range(rockAmountMin, rockAmountMax);
+        GenerateAssets(plantPrefabs, plantAmount, plantScaleMin, plantScaleMax);
     }
 
     // Update is called once per frame
@@ -20,39 +45,30 @@ public class GenerateTerrainAssets : MonoBehaviour
         
     }
 
-    void GenerateAssets(){
+    void GenerateAssets(GameObject[] prefabs, float amountToGenerate, float scaleMin, float scaleMax){
         //Active terrain, should only be 1
         RaycastHit rayHit;
-        //Random amount for whatever we are spawning
-        int randomMin = Random.Range(100,200);
-        int randomMax = Random.Range(300,500);
         TerrainGenerator terrainScript = GetComponent<TerrainGenerator>();
         Terrain terrain = Terrain.activeTerrain;
         Vector3 terrainPos = terrain.transform.position;
         Bounds terrainBounds = terrain.terrainData.bounds;
-        for(int i = 0; i < terrainPrefabs.Length; i++){
+        for(int i = 0; i < prefabs.Length; i++){
             //Get random amount to generate
-            int randomNum = Random.Range(randomMin,randomMax);
-            GameObject terrainPrefab = terrainPrefabs[i];
-            for(int j = 0; j < randomNum; j ++){
+            GameObject terrainPrefab = prefabs[i];
+            float randomScale = Random.Range(scaleMin, scaleMax);
+            for(int j = 0; j < amountToGenerate; j ++){
                 Vector3 randomPos = new Vector3();
                 Vector3 terrainMin = terrain.terrainData.bounds.min;
                 Vector3 terrainMax = terrain.terrainData.bounds.max;
                 randomPos.x = UnityEngine.Random.Range(terrainPos.x + terrainMin.x, terrainPos.x + terrainMax.x);
                 randomPos.z = UnityEngine.Random.Range(terrainPos.z + terrainMin.z, terrainPos.z + terrainMax.z);
                 randomPos.y = terrain.SampleHeight(new Vector3(randomPos.x,0,randomPos.z )) + terrain.transform.position.y + 1.5f;
-
-                // Vector3 randomPos = Vector3.Lerp(randomWorldPosMin, randomWorldPosMax, UnityEngine.Random.value);
-                //Convert randomPos coords to normalized terrain cords, assuming it is in the terrain
-                // Vector3 randomPosNormalized = terrainScript.WorldToTerrain(terrain, randomPos);
-                // Vector3 randomPosClamped = terrainScript.clampPositionWithinTerrain(terrain, randomPosNormalized);
-                // //Convert normalized clamped position to world position
-                // Vector3 randomPosWorld = terrainScript.TerrainToWorld(terrain, randomPosClamped);
                 //Raycast downwards, get the spot that is hit
                 Physics.Raycast(randomPos, Vector3.down,  out rayHit);
                 randomPos = rayHit.point;
                 // Generate Enemy
                 GameObject terrainAsset = Instantiate(terrainPrefab, randomPos, Quaternion.identity, TerrainAssetsParent.transform);
+                terrainAsset.transform.localScale = new Vector3(randomScale,randomScale,randomScale);
             }
         }
     }
