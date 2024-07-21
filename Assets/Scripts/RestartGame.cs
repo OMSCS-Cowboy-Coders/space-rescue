5e2d2eb6fb691e71682e7356dbacac0c9730e1f2
Christian Tran <ctran4347@gmail.com>
2024-07-21 13:04:44 -0500

    Remove comments

--- Assets/Scripts/RestartGame.cs
+++ Assets/Scripts/RestartGame.cs
@@ -8,10 +8,6 @@ public class RestartGame : MonoBehaviour
 {
     public static string sceneName = "Mars_Scene"; 
 
-    // public void StartGame()
-    // {
-    //     Restart();
-    // }
     public static void Restart() {
         SceneManager.LoadScene(sceneName);
         Time.timeScale = 1f;
